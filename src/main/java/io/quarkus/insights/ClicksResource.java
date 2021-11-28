4d5c86e2a530d4ef8b3f1d4038775104058b001b
Ozan Gunalp <ozangunalp@gmail.com>
2021-11-28 20:11:35 +0000

    Produce Kafka messages with emitter

--- src/main/java/io/quarkus/insights/ClicksResource.java
+++ src/main/java/io/quarkus/insights/ClicksResource.java
@@ -6,7 +6,11 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.jboss.logging.Logger;
+
+import io.smallrye.mutiny.Uni;
+import io.smallrye.reactive.messaging.MutinyEmitter;
 
 @Path("/clicks")
 public class ClicksResource {
@@ -14,9 +18,13 @@ public class ClicksResource {
     @Inject
     Logger logger;
 
+    @Channel("clicks")
+    MutinyEmitter<Click> clicksEmitter;
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public void postClick(Click click) {
+    public Uni<Void> postClick(Click click) {
         logger.infof("Posted click %s", click);
+        return clicksEmitter.send(click);
     }
 }
